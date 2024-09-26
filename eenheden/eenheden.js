84417c41fc36488132036ea45fa164e681a51f11
Ids Osinga <ip.osinga@alfa-college.nl>
2024-09-26 12:02:20 +0200

    v2024-09-26

--- eenheden/eenheden.js
+++ eenheden/eenheden.js
@@ -1,8 +1,33 @@
-let dimension = document.getElementById('dim_select');
-let dim =dimension.value;
-console.log("dimensie = " + dim)
+//array (rij) is een aantal variabelen in 1 variabele; meestal hebben ze veel met elkaar
+//te maken; bij het klikken op 'NIEUW' moet er willekeurig eentje uit deze array worden
+//gepakt;
+//als je dus een willekeurig cijfer tussen 0 en 6 genereert en die gebruik je als
+//indexwaarde voor eenheden, dan heb je een willekeurige eenheid gekozen!
+const eenheden = ["mm", "cm", "dm", "m", "dam", "hm", "km"];
+// index            0     1     2    3     4      5     6
 
-function hallo()
+
+let dimension = document.getElementById("dimselect");
+let opg = document.getElementById("opg_input");
+let opgeenh = document.getElementById("opg_eenheid");
+let antw = document.getElementById("antw_input");
+let antweenh = document.getElementById("antw_eenheid");
+
+function nieuw()
 {
-    alert("Hallo")
+    let dim = dimension.value
+    //Math.random genereert een getal tussen 0 en 1, waarbij 0 wel kan, maar 1 niet
+    //door *7 te doen, krijg je een getal tussen 0 en 7
+    let randomnmbr = Math.floor(Math.random() * 7);
+    opgeenh.innerHTML = eenheden[randomnmbr] + "<sup>" + dim + "</sup>";
+    //console.log("randomnmbr = " + randomnmbr)
+    //console.log("de gekozen dimensie = " + dim);
+    //console.log("het gekozen element uit eenheden is: " + eenheden[randomnmbr])
+    //het programma moet zijn werk gaan doen......
+    
+}
+
+function check()
+{
+
 }
